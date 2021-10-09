c684323686e2eeac49e7b6d92189dd91a1ab633c
SoQkingdom <72286607+SoQkingdom@users.noreply.github.com>
2021-10-09 21:21:13 +0800

    Update TestUser.java

--- src/com/atguigu/spring5/test/TestUser.java
+++ src/com/atguigu/spring5/test/TestUser.java
@@ -27,5 +27,6 @@ public class TestUser {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("github update!");
     }
 }
